f7a0d43474c886be5e994f3495717839710fe4e1
Kuvakin Sergey <kuvakin@gmail.com>
2015-08-28 16:26:22 +0300

    refs #7681 added test for cache

---
+++ tests/CacheTest.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once __DIR__ . '/../lib/Innometrics/Cache.php';
+
+use Innometrics\Cache;
+
+class CacheTest extends PHPUnit_Framework_TestCase {
+
+    protected function createCache ($config = array()) {
+        return new Cache($config);
+    }
+
+    /**
+     * @expectedException        ErrorException
+     * @expectedExceptionMessage Config should be an object
+     */
+    public function testShouldBeReturnErrorIfMethodGenerateCallWithConfigDontArrayType () {
+        $this->createCache((object)array());
+    }
+
+    public function testShouldMakeForgetProperty () {
+        $cache = $this->createCache();
+        $cache->setCachedTime(10);
+        $cache->set('name1', 'value1');
+        $this->assertEquals('value1', $cache->get('name1'), 'get value before forget test');
+        $cache->expire('name1');
+        $this->assertEquals(NULL, $cache->get('name1'), 'get value before forget test');
+    }
+
+}
