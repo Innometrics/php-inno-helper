13e71867beeefd379460336385777cd9af43986f
Kuvakin Sergey <kuvakin@gmail.com>
2015-08-31 12:51:38 +0300

    refs #8433 partial implementation test for Sessions

--- tests/ProfileTest.php
+++ tests/ProfileTest.php
@@ -5,13 +5,16 @@ require_once __DIR__ . '/../lib/Innometrics/Profile.php';
 use Innometrics\Profile;
 
 class ProfileTest extends PHPUnit_Framework_TestCase {
-    
+
     protected function createProfile ($config = array()) {
         return new Profile($config);
     }
-    
-    public function testShouldNotThrowErrorOnEmptyConfig () {
-        $this->createProfile();
+
+    public function testShouldBeRetirnId () {
+        $profile = $this->createProfile(array(
+            'id' => 'pid'
+        ));
+        $this->assertEquals('pid', $profile->getId());
     }
 
 }
--- tests/SessionTest.php
+++ tests/SessionTest.php
@@ -10,8 +10,71 @@ class SessionTest extends PHPUnit_Framework_TestCase {
         return new Session($config);
     }
 
-    public function testShouldThrowErrorOnEmptyConfig () {
-        $this->createSession();
+    public function testShouldNotThrowErrorOnIsValid () {
+        $session = $this->createSession(array(
+            'collectApp' => 'collectApp1',
+            'section' => 'section1',
+            'data' => array('name1' => 'value1'),
+            'events' => array(
+                array(
+                    'definitionId' => 'name1',
+                    'data' => array('name1' => 'value1')
+                ),
+                array(
+                    'definitionId' => 'name2',
+                    'data' => array('name2' => 'value2')
+                )
+            )
+        ));
+        $this->assertTrue($session->isValid());
+    }
+
+    public function testShouldReceiveMergedData () {
+        $session1 = $this->createSession(array(
+            'id' => 'sid1',
+            'collectApp' => 'collectApp1',
+            'section' => 'section1',
+            'data' => array('name1' => 'value1'),
+            'events' => array(
+                array(
+                    'id' => 'eid1',
+                    'definitionId' => 'name1',
+                    'data' => array('name1' => 'value1')
+                ),
+                array(
+                    'definitionId' => 'name2',
+                    'data' => array('name2' => 'value2')
+                )
+            )
+        ));
+
+        $session2 = $this->createSession(array(
+            'id' => 'sid1',
+            'collectApp' => 'collectApp1',
+            'section' => 'section1',
+            'data' => array('name1' => 'value1'),
+            'events' => array(
+                array(
+                    'id' => 'eid1',
+                    'definitionId' => 'name1',
+                    'data' => array('name1' => 'value1')
+                ),
+                array(
+                    'definitionId' => 'name3',
+                    'data' => array('name3' => 'value3')
+                )
+            )
+        ));
+
+        $session1->merge($session2);
+        $this->assertEquals(count($session1->getEvents()), 3, 'merge test');
+
+        $session2->setId('sid2');
+        try {
+            $session1->merge($session2);
+        } catch (\Exception $e) {
+            $this->assertEquals('Session IDs should be similar', $e->getMessage());
+        }
     }
 
 }
